09646e889c7bfabaeb8a5289aab6895ad04946d0
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-01-16 21:28:13 -0500

    creando parametros y por defecto yargs

--- 02-bases-node/02-package-json/app.js
+++ 02-bases-node/02-package-json/app.js
@@ -3,7 +3,34 @@ const argv=require('yargs')
           .option('b',{
             alias: 'base',
             type: 'number',
+            demandOption: true,
           })
+          .check( (argv, options)=>{
+            if (isNaN(argv.b)){
+              throw "La base debe ser un numero"
+            }
+            return true;
+          })
+          .option(
+            'l',{
+              alias: 'limite',
+              type: 'number',
+              demandOption: true,
+            }
+          )
+          .check( (argv, options)=>{
+            if (isNaN(argv.l)){
+              throw "El limite debe ser un numero"
+            }
+            return true;
+          })
+          .option(
+            'e',{
+              alias: 'ejecutar',
+              type: 'boolean',
+              default:false
+            }
+          )
           .argv;
 console.clear();
 
@@ -11,9 +38,10 @@ console.log(argv);
 // cuando se pasa por consola el crear un objeto y es mas facil de utilizar
 console.log(argv.base);
 
-// const base=18;
-// const limite =17;
+const base=argv.b;
+const limite =argv.l;
+const ejecutar=argv.e
 
-// crearArchivoTabal(base,limite)
-// .then(arc=> console.log(arc,'creado'))
-// .catch(err=> console.log(err));
+crearArchivoTabal(base,limite,ejecutar)
+.then(arc=> console.log(arc,'creado'))
+.catch(err=> console.log(err));
--- 02-bases-node/02-package-json/helpers/multiplication.js
+++ 02-bases-node/02-package-json/helpers/multiplication.js
@@ -1,11 +1,15 @@
 const fs=require('fs');
 // se crea la funcion para crear archivos y guardar la info
-const crearArchivoTabal=async(base=5, limite=10)=>{
-    let salida='';
-    for (let i = 1; i <=limite; i++){
-      salida += `${base}x${i}=${base*i} \n`
-    }
+const crearArchivoTabal=async(base=5, limite=10,ejecutar=false)=>{
+
     try {
+      let salida='';
+      for (let i = 1; i <=limite; i++){
+        salida += `${base}x${i}=${base*i} \n`  
+      }
+      if(ejecutar){
+        console.log(salida);
+      }
       fs.writeFileSync(`tabla${base}.csv`,salida);
       return `tabla${base}.csv`;
     } catch (error) {
