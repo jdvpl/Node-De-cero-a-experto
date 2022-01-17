0e04c9c8f4ca5a5c89cacc09dafd6f50197b83d5
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-01-16 22:16:33 -0500

    salida de archivos en una carpeta y colores en consola

--- 02-bases-node/02-package-json/app.js
+++ 02-bases-node/02-package-json/app.js
@@ -1,15 +1,15 @@
-const {crearArchivoTabal}=require('./helpers/multiplication')
-const argv=require('./config/yargs')
+const {crearArchivoTabal}=require('./helpers/multiplication');
+const argv=require('./config/yargs');
+require('colors');
 console.clear();
 
 console.log(argv);
 // cuando se pasa por consola el crear un objeto y es mas facil de utilizar
-console.log(argv.base);
 
 const base=argv.b;
 const limite =argv.l;
 const ejecutar=argv.e
 
 crearArchivoTabal(base,limite,ejecutar)
-.then(arc=> console.log(arc,'creado'))
+.then(arc=> console.log(arc.rainbow,'creado'))
 .catch(err=> console.log(err));
--- 02-bases-node/02-package-json/helpers/multiplication.js
+++ 02-bases-node/02-package-json/helpers/multiplication.js
@@ -1,15 +1,20 @@
 const fs=require('fs');
+const colors=require('colors');
 // se crea la funcion para crear archivos y guardar la info
 const crearArchivoTabal=async(base=5, limite=10,ejecutar=false)=>{
     try {
-      let salida='';
+      let salida,consola='';
       for (let i = 1; i <=limite; i++){
-        salida += `${base}x${i}=${base*i} \n`  
+        consola += `${base}${'X'.green}${i}${'='.green}${base*i} \n`;  
+        salida += `${base}X${i}=${base*i}\n`;  
       }
       if(ejecutar){
+        console.log("=============================================".green);
+        console.log("Tabla del:".blue,colors.white(base));
+        console.log("=============================================".green);
         console.log(salida);
       }
-      fs.writeFileSync(`tabla${base}.csv`,salida);
+      fs.writeFileSync(`./salida/tabla${base}.txt`,salida);
       return `tabla${base}.csv`;
     } catch (error) {
       throw error;
