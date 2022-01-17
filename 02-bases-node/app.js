264c8ef4188a174b4bbb0240a1e2e8816ec43888
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-01-16 20:07:14 -0500

    pasando variables por consola aplicaciones de consola  forma incorrecta

--- 02-bases-node/app.js
+++ 02-bases-node/app.js
@@ -4,8 +4,16 @@ const {crearArchivoTabal}=require('./helpers/multiplication')
 
 console.clear();
 
-const base=18;
-const limite =17;
+
+console.log(process.argv);
+const [,,b='base=5',l='limite=10']=process.argv;
+const [,base=5]=b.split("=")
+const [,limite=10]=l.split("=")
+
+
+// const base=18;
+// const limite =17;
+
 crearArchivoTabal(base,limite)
-.then(arc=> console.log(arc,'Creado'))
+.then(arc=> console.log(arc,'creado'))
 .catch(err=> console.log(err));
